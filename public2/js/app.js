1daad33494edbcd018ae86b2bcea671ce919975f
unknown <ranabhatprakash72@gmail.com>
2021-01-31 15:19:19 +1100

    Setup app fpr heroku

--- public2/js/app.js
+++ public2/js/app.js
@@ -1,10 +1,5 @@
 console.log('client side js is loading')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) =>  {
-        console.log(data)
-    })
-})  
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -20,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error) {
                 messageOne.textContent = data.error
